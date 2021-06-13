f2bcec285b5939b3f187763d0f506b59dd5c8307
Wander Aquino <wander.aquino@gmail.com>
2021-06-13 01:05:06 -0300

    Adicionando componente e CSS Calculator

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import "../css/Calculator.css";
+import React from "react";
+
+export default class Calculator extends React.Component {
+    render() {
+        return (
+            <div className="calculator">
+                {/* Demais componentes, como display, botões etc. */}
+            </div>
+        )
+    }
+}
+    
