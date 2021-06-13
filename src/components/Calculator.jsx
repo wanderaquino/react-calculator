9ac8e2da3f64d0623b4e764be187949caafff38a
Wander Aquino <wander.aquino@gmail.com>
2021-06-13 20:05:20 -0300

    Incluindo todos os botões da calculadora

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,12 +8,27 @@ export default class Calculator extends React.Component {
         return (
             <div className="calculator">
                 <Display value="0"></Display>
-                <Button value="+" type="OP"></Button>
-                <Button value="1" type="NM"></Button>
-                <Button value="AC" type="FN"></Button>
+                <Button value="AC" type="function"></Button>
+                <Button value="+/-" type="function"></Button>
+                <Button value="%" type="function"></Button>
+                <Button value="/" type="operation"></Button>
+                <Button value="7" type="number"></Button>
+                <Button value="8" type="number"></Button>
+                <Button value="9" type="number"></Button>
+                <Button value="*" type="operation"></Button>
+                <Button value="4" type="number"></Button>
+                <Button value="5" type="number"></Button>
+                <Button value="6" type="number"></Button>
+                <Button value="-" type="operation"></Button>
+                <Button value="1" type="number"></Button>
+                <Button value="2" type="number"></Button>
+                <Button value="3" type="number"></Button>
+                <Button value="+" type="operation"></Button>
+                <Button value="0" type="number" double="double"></Button>
+                <Button value="." type="number"></Button>
+                <Button value="=" type="number"></Button>
                 {/* Demais componentes, como display, botões etc. */}
             </div>
         )
     }
 }
-    
