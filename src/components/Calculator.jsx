2ebeaf6f5432b511ee7edfcaff46ed89ecf9148d
Wander Aquino <wander.aquino@gmail.com>
2021-06-13 18:21:26 -0300

    Atualizando componente calculadora para inclusão dos botões.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,16 @@
 import "../css/Calculator.css";
 import React from "react";
+import Display from "./Display";
+import Button from "./Button";
 
 export default class Calculator extends React.Component {
     render() {
         return (
             <div className="calculator">
+                <Display value="0"></Display>
+                <Button value="+" type="OP"></Button>
+                <Button value="1" type="NM"></Button>
+                <Button value="AC" type="FN"></Button>
                 {/* Demais componentes, como display, botões etc. */}
             </div>
         )
